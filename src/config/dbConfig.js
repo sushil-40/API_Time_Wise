17b10bd178bb652822885f48c11f21b060a3cada
Sushil <grayson.9999@gmail.com>
2025-02-26 10:15:46 +1000

    changing connection to the mongo cloud

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -11,8 +11,9 @@
 
 import mongoose from "mongoose";
 
-const mongoUrl = "mongodb://localhost:27017/online_time_wise";
-
+// const mongoUrl = "mongodb://localhost:27017/online_time_wise";
+const mongoUrl =
+  "mongodb+srv://grayson9999:sushil123@cluster-time-wise-db.cieql.mongodb.net/time_wise?retryWrites=true&w=majority&appName=Cluster-time-wise-db";
 export const connectMongoDb = async () => {
   try {
     const conn = await mongoose.connect(mongoUrl);
